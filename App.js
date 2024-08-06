a94cb822ecff84bfa6ca3e7828f6250c2bc47223
ShivanshuShekhar <shivanshushekhar786@gmail.com>
2024-08-06 15:02:23 +0530

    Using JSX in React

--- App.js
+++ App.js
@@ -5,6 +5,10 @@ import ReactDOM from 'react-dom/client';
 
 const heading = React.createElement("h1", {id: "heading"}, "Namaste React");
 
+// JSX
+
+const jsxHeading = <h1>Namaste React using JSX</h1>;
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(heading);
