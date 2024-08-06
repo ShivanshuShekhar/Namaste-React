25d5d2c6c5f29eed713b6a839855d8d220a8a2bc
ShivanshuShekhar <shivanshushekhar786@gmail.com>
2024-08-06 14:48:41 +0530

    Revising Old Chapters

--- App.js
+++ App.js
@@ -1,25 +1,10 @@
 import React from "react";
 import ReactDOM from 'react-dom/client';
 
-const parent = React.createElement(
-    'div', 
-    { id: 'parent' }, 
-    [React.createElement(
-        'div',
-        {id: 'child'},
-        [React.createElement('h1', {}, "I'm an H1 tag"),
-        React.createElement('h2', {}, "I'm an H2 tag")
-    ]),
-    React.createElement(
-        'div',
-        {id: 'child2'},
-        [React.createElement('h1', {}, "I'm an H1 tag"),
-        React.createElement('h2', {}, "I'm an H2 tag")
-    ])]
-);
+// React.createElement => Object => HTMLElement(render)
 
-console.log(parent); //object
+const heading = React.createElement("h1", {id: "heading"}, "Namaste React");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(heading);
--- dist/index.7826abd7.js
+++ dist/index.7826abd7.js
@@ -2956,25 +2956,12 @@ var _react = require("react");
 var _reactDefault = parcelHelpers.interopDefault(_react);
 var _client = require("react-dom/client");
 var _clientDefault = parcelHelpers.interopDefault(_client);
-const parent = /*#__PURE__*/ (0, _reactDefault.default).createElement("div", {
-    id: "parent"
-}, [
-    /*#__PURE__*/ (0, _reactDefault.default).createElement("div", {
-        id: "child"
-    }, [
-        /*#__PURE__*/ (0, _reactDefault.default).createElement("h1", {}, "I'm an H1 tag"),
-        /*#__PURE__*/ (0, _reactDefault.default).createElement("h2", {}, "I'm an H2 tag")
-    ]),
-    /*#__PURE__*/ (0, _reactDefault.default).createElement("div", {
-        id: "child2"
-    }, [
-        /*#__PURE__*/ (0, _reactDefault.default).createElement("h1", {}, "I'm an H1 tag"),
-        /*#__PURE__*/ (0, _reactDefault.default).createElement("h2", {}, "I'm an H2 tag")
-    ])
-]);
-console.log(parent); //object
+// React.createElement => Object => HTMLElement(render)
+const heading = /*#__PURE__*/ (0, _reactDefault.default).createElement("h1", {
+    id: "heading"
+}, "Namaste React");
 const root = (0, _clientDefault.default).createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(heading);
 
   $parcel$ReactRefreshHelpers$c1db.postlude(module);
 } finally {
